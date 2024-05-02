790e98ac4db2e9bea4bcb9c3443b22bf7c64a008
ZubaidurBagmar99 <zubaidur99@gmail.com>
2024-05-02 20:55:12 +0600

    db update

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,12 +1,13 @@
 const dbConnect = () => {
   const mongoose = require("mongoose");
 
-  mongoose.connect("mongodb://localhost:27017").then(
+  mongoose.connect("mongodb://127.0.0.1:27017").then(
     () => {
       console.log("connect db successful");
     },
     (err) => {
       /** handle initial connection error */
+      console.log("connect db  error");
     }
   );
 };
